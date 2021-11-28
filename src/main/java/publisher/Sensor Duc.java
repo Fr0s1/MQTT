fa24a17c4f7bd8eee8fb1441647e3501dbc37491
Payaso2767 <56871492+Payaso2767@users.noreply.github.com>
2021-11-28 14:26:02 +0700

    fixxxxxxxxx

--- src/main/java/publisher/Sensor Duc.java
+++
@@ -1,8 +0,0 @@
-package publisher;
-import org.json.JSONObject;
-public class Sensor {
-    public static void main(String[] args) {
-        JSONObject jo = new JSONObject("{ \"abc\" : \"def\" }");
-        System.out.println(jo.toString());
-    }
-}
