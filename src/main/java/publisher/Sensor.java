ac04fac80147a9a3dbd976d203890f64d1112801
Payaso2767 <56871492+Payaso2767@users.noreply.github.com>
2021-12-08 22:18:17 +0700

    Random data in sensor

--- src/main/java/publisher/Sensor.java
+++ src/main/java/publisher/Sensor.java
@@ -1,11 +1,13 @@
 package publisher;
 
+import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.Random;
 import java.net.*;
 import java.io.*;
+import java.util.Random;
 import java.util.Scanner;
+import org.json.JSONArray;
+import org.json.JSONTokener;
 
 public class Sensor {
     public final static int sensor = 1;
@@ -17,8 +19,12 @@ public class Sensor {
         PUBLISH_DATA
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         String receive;
+        String resourceName = System.getProperty("user.dir") + "\\src\\main\\java\\publisher\\csvjson.json";
+        Random rd = new Random();
+        JSONTokener tokener = new JSONTokener(new FileReader(resourceName));
+        JSONArray array = new JSONArray(tokener);
         Scanner sc = new Scanner(System.in);
         System.out.print("Input from client - location: ");
         String location = sc.nextLine();
@@ -27,7 +33,6 @@ public class Sensor {
 //        System.out.print("Input from client - PORT: ");
 //        int PORT = sc.nextInt();
 //        sc.nextLine();
-        String sent;
         State state = State.HANDSHAKE;
 
         try {
@@ -44,11 +49,32 @@ public class Sensor {
                     sentBuff.writeUTF(jo.toString());
                     state = State.PUBLISH_DATA;
                 } else if (state == State.PUBLISH_DATA) {
-                    System.out.print("Input from client: ");
-
-                    sent = sc.nextLine();
-                    if (sent.length() != 0) {
-                        sentBuff.writeUTF(sent);
+                    int n = rd.nextInt(array.length());
+                    System.out.println("Random: "+ n );
+                    JSONObject object1 = array.getJSONObject(n);
+                    try{
+                        System.out.println("Temperature  : " + object1.getString("Air Temperature"));
+                    } catch(JSONException ex ) {
+                        System.out.println("Temperature  : " + object1.getInt("Air Temperature"));
+                    }
+                    try{
+                        System.out.println("Rain Intensity: " + object1.getInt("Rain Intensity"));
+                    } catch(JSONException ex ) {
+                        System.out.println("Rain Intensity: " + object1.getString("Rain Intensity"));
+                    }
+                    try{
+                        System.out.println("Solar Radiation : " + object1.getInt("Solar Radiation"));
+                    } catch(JSONException ex ) {
+                        System.out.println("Solar Radiation : " + object1.getString("Solar Radiation"));
+                    }
+                    sentBuff.writeUTF(object1.toString());
+                    try
+                    {
+                        Thread.sleep(1500);
+                    }
+                    catch(InterruptedException ex)
+                    {
+                        Thread.currentThread().interrupt();
                     }
                 }
                 receive = recBuff.readUTF();
