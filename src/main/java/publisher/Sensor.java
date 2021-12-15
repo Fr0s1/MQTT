f80f0ec44eae94c73c7dd1f1d69a78850397b528
Payaso2767 <56871492+Payaso2767@users.noreply.github.com>
2021-12-15 23:12:59 +0700

    Clean code and add comment

--- src/main/java/publisher/Sensor.java
+++ src/main/java/publisher/Sensor.java
@@ -2,11 +2,13 @@ package publisher;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.net.*;
 import java.io.*;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.Date;
+
 import org.json.JSONArray;
 import org.json.JSONTokener;
 
@@ -15,6 +17,7 @@ public class Sensor {
     public final static String type = "temperature";
     public final static String MAC = "11:22:33:44:55:66";
 
+    // Set State for Sensor
     enum State {
         HANDSHAKE,
         PUBLISH_DATA
@@ -22,18 +25,13 @@ public class Sensor {
 
     public static void main(String[] args) throws FileNotFoundException {
         String receive;
-        String resourceName = System.getProperty("user.dir") + "\\src\\main\\java\\publisher\\csvjson.json";
+        String resourceName = System.getProperty("user.dir") + "\\src\\main\\java\\publisher\\csvjson.json";// Set location of json data
         Random rd = new Random();
         JSONTokener tokener = new JSONTokener(new FileReader(resourceName));
         JSONArray array = new JSONArray(tokener);
         Scanner sc = new Scanner(System.in);
         System.out.print("Input from client - location: ");
         String location = sc.nextLine();
-//        System.out.print("Input from client - HOSTNAME: ");
-//        String HOSTNAME = sc.nextLine();
-//        System.out.print("Input from client - PORT: ");
-//        int PORT = sc.nextInt();
-//        sc.nextLine();
         State state = State.HANDSHAKE;
 
         try {
@@ -42,6 +40,7 @@ public class Sensor {
             DataInputStream recBuff = new DataInputStream(new BufferedInputStream(connection.getInputStream()));
             while (true) {
                 if (state == State.HANDSHAKE) {
+                    // Check Status of sensor for Handshake
                     JSONObject jo = new JSONObject();
                     jo.put("sensor", sensor);
                     jo.put("MAC", MAC);
@@ -49,18 +48,16 @@ public class Sensor {
                     sentBuff.writeUTF(jo.toString());
                     state = State.PUBLISH_DATA;
                 } else if (state == State.PUBLISH_DATA) {
+                    // Check Status of sensor for Publish data
                     int n = rd.nextInt(array.length());
-                    System.out.println("Random: "+ n );
+                    System.out.println("Random: " + n);// Get Random Data
                     JSONObject object1 = array.getJSONObject(n);
                     String time = (java.time.Clock.systemUTC().instant()).toString();
-                    object1.put("sent_time", time);
-                    sentBuff.writeUTF(object1.toString());
-                    try
-                    {
-                        Thread.sleep(1500);
-                    }
-                    catch(InterruptedException ex)
-                    {
+                    object1.put("sent_time", time);// Add time on json
+                    sentBuff.writeUTF(object1.toString());//Sent data to Server
+                    try {
+                        Thread.sleep(5000);// Delay pushing data
+                    } catch (InterruptedException ex) {
                         Thread.currentThread().interrupt();
                     }
                 }
